a244c9a62d21371972390eb144c4ac4dd1bab811
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2022-05-18 08:08:35 +0200

    Update circulating_currents_calculation.py

--- circulating_currents_calculation.py
+++ circulating_currents_calculation.py
@@ -29,3 +29,22 @@ Ic_ret
 grupa_spoja = np.array([['Yy', 0.0], ['Dd', 0], ['Dz', 0], ['Yd', 1], ['Dy', 1], ['Dz', 1], ['Dd', 2], ['Dz', 2], ['Dd', 4], ['Dz', 4], ['Yd', 5], ['Dy', 5], ['Yz', 5], ['Yy', 6], ['Dd', 6], ['Dz', 6], ['Yd',7], ['Dy', 7], ['Yz', 7], ['Dd', 8], ['Dz', 8], ['Dd', 10], ['Dz', 10], ['Yd' , 11], ['Dy', 11], ['Yz', 11]])
 len(grupa_spoja)
 grupa_spoja
+
+lista_kuteva = np.array(range(676)).reshape((26, 26)) 
+
+for i in range(len(grupa_spoja)):
+  alfa1 = (float(grupa_spoja[i][1])*30)
+  for j in range(len(grupa_spoja)):
+    alfa2 = (float(grupa_spoja[j][1])*30)
+    alfa = alfa2-alfa1
+    lista_kuteva[i][j] = np.abs(alfa)
+
+index =['Yy0', 'Dd0', 'Dz0', 'Yd1', 'Dy1', 'Dz1', 'Dd2', 'Dz2', 'Dd4', 'Dz4', 'Yd5', 'Dy5', 'Yz5', 'Yy6', 'Dd6', 'Dz6', 'Yd7', 'Dy7', 'Yz7', 'Dd8', 'Dz8', 'Dd10', 'Dz10', 'Yd11', 'Dy11', 'Yz11']
+
+df = pd.DataFrame(lista_kuteva, columns= index, index=index)
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+display(df)
